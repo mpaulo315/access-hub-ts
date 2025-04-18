5849272738bf197ac8c596e1ae5ee0d19b524487
Marcos Paulo <mpaulosp.315@gmail.com>
2025-04-18 14:58:34 -0300

    feat: Pinia and ToastService added

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,19 @@ import { createApp } from "vue";
 import PrimeVue from "primevue/config";
 import Aura from "@primeuix/themes/aura";
 import App from "./App.vue";
-import { Tooltip } from "primevue";
+import { ToastService, Tooltip } from "primevue";
 import axios from "axios";
 
 import "./style.css";
 import router from "./routers";
+import { createPinia } from "pinia";
 
 axios.defaults.withCredentials = true;
+const Pinia = createPinia();
 
 const app = createApp(App);
 app.directive("tooltip", Tooltip);
+
 app.use(PrimeVue, {
   theme: {
     preset: Aura,
@@ -22,6 +25,11 @@ app.use(PrimeVue, {
     },
   },
 });
+
 app.use(router);
+
+app.use(Pinia);
+
+app.use(ToastService);
 
 app.mount("#app");
